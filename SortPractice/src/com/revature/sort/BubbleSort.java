da3bf436464618c168c9f2b1e0ff2c92dcc30eed
Dane Vosmeier <dane.vosmeier@revature.net>
2019-11-05 13:37:13 -0500

    pretty sure this will work

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -3,6 +3,28 @@ package com.revature.sort;
 public class BubbleSort {
 	
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+		//find largest value
+		//put largest value to the end
+		
+		int temp = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			int min = arr[i];
+			int minId = i;
+			
+			
+			for(int j = i + 1; j < arr.length; j++) {
+				
+				if(arr[j] > min) {
+					min = arr[j];
+					minId = j;
+				}
+			}
+			
+			temp = arr[i];
+			arr[i] = min;
+			arr[minId] = temp;
+		}
 	}
 }
